4ef08288e77850fd57a21e52a1a115aa6cad39a8
ttellier <thomas.tellier.1@ens.etsmtl.ca>
2025-10-02 15:36:50 -0400

    Smoke Test 1

--- src/tests/test_store_manager.py
+++ src/tests/test_store_manager.py
@@ -31,8 +31,46 @@ def test_stock_flow(client):
     assert data['product_id'] > 0 
 
     # 2. Ajoutez 5 unités au stock de cet article (`POST /stocks`)
+    product_id = data['product_id']
+    stock = {'product_id': product_id, 'quantity': 5}
+    response = client.post('/stocks',
+                            data=json.dumps(stock),
+                            content_type='application/json')
+    
+    assert response.status_code == 201
+
     # 3. Vérifiez le stock, votre article devra avoir 5 unités dans le stock (`GET /stocks/:id`)
+    response = client.get(f'/stocks/{product_id}')
+    assert response.status_code == 200
+    data = response.get_json()
+    assert data['quantity'] == 5
+
     # 4. Faites une commande de l'article que vous avez crée, 2 unités (`POST /orders`)
+    item_data = {'product_id': product_id, 'quantity': 2}
+    order_data = {'user_id': 1, 'items': [item_data]}
+    response = client.post('/orders',
+                           data=json.dumps(order_data),
+                           content_type='application/json')
+    
+    assert response.status_code == 201
+    data = response.get_json()
+    assert data['order_id'] > 0 
+
     # 5. Vérifiez le stock encore une fois (`GET /stocks/:id`)
+
+    response = client.get(f'/stocks/{product_id}')
+    assert response.status_code == 200
+    data = response.get_json()
+    assert data['quantity'] == 3
+
     # 6. Étape extra: supprimez la commande et vérifiez le stock de nouveau. Le stock devrait augmenter après la suppression de la commande.
-    assert "Le test n'est pas encore là" == 1
+    order_id = data['order_id']
+    response = client.delete(f'/orders/{order_id}')
+    assert response.status_code == 200
+
+    response = client.get(f'/stocks/{product_id}')
+    assert response.status_code == 200  
+    data = response.get_json()
+    assert data['quantity'] == 5
+    
+    #assert "Le test n'est pas encore là" == 1
